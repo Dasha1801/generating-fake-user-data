490787fbe09b34690812d40962d3551f620921ab
Dasha1801 <6227968@gmail.com>
2021-11-05 00:06:36 +0300

    feat: add endless scrolling

--- client/src/components/table/table.jsx
+++ client/src/components/table/table.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import Data from "./data/data";
 
 const TableUsers = () => {
   const dbEn = useSelector(({ dbEn }) => dbEn);
+  const [countRow, setCountRow] = useState(20);
 
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+    return function () {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, [countRow]);
   // console.log(dbEn);
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setCountRow(countRow + 10);
+    }
+  };
 
   return (
     <Table striped bordered hover size="sm">
@@ -20,7 +37,7 @@ const TableUsers = () => {
       </thead>
       <tbody>
         {dbEn.length
-          ? dbEn.map((user) => {
+          ? dbEn.slice(0, countRow).map((user) => {
               return <Data user={user} key={user.number} />;
             })
           : null}
