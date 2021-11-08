126edf7bec01c87eb85a7c9794e4aa8865581be7
Dasha1801 <75580151+Dasha1801@users.noreply.github.com>
2021-11-08 09:45:02 +0300

    Add method for mysql.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,51 @@
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-const db = mysql.createConnection({
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://task-5.netlify.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const db_config = {
   user: "b5adcc5cb9c065",
   host: "eu-cdbr-west-01.cleardb.com",
   password: "d2a0bb74",
   database: "heroku_1c4d5625035b043",
-});
+};
+
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+handleDisconnect();
 
 app.post("/", (rej, res) => {
   const name = rej.body.name;
@@ -32,8 +66,8 @@ app.post("/", (rej, res) => {
   );
 });
 
-app.get("/usa", (req, res) => {
-  db.query("SELECT * FROM users_usa", (err, result) => {
+app.get("/ru", (req, res) => {
+  connection.query("SELECT * FROM users_ru", (err, result) => {
     if (err) {
       console.log(err);
     } else {
@@ -42,8 +76,8 @@ app.get("/usa", (req, res) => {
   });
 });
 
-app.get("/ru", (req, res) => {
-  db.query("SELECT * FROM users_ru", (err, result) => {
+app.get("/usa", (req, res) => {
+  connection.query("SELECT * FROM users_usa", (err, result) => {
     if (err) {
       console.log(err);
     } else {
@@ -53,7 +87,7 @@ app.get("/ru", (req, res) => {
 });
 
 app.get("/ua", (req, res) => {
-  db.query("SELECT * FROM users_ua", (err, result) => {
+  connection.query("SELECT * FROM users_ua", (err, result) => {
     if (err) {
       console.log(err);
     } else {
@@ -62,6 +96,8 @@ app.get("/ua", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
+app.set("port", process.env.PORT || 3000);
+
+app.listen(PORT, () => {
   console.log("Yey, your server is running on port 3001");
 });
